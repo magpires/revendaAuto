d9e76f33fa50ebadc511d33378af064fc16b5e37
Weverton <w.m.pires@hotmail.com>
2020-06-25 15:12:17 -0300

    4. Criando Registros de Testes com Tinker

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,15 +3,13 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+        <h2>Lista de chamados</h2>
 
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+        @forelse ($chamados as $chamado)
+            <p>{{$chamado->titulo}}</p>
+        @empty
+            <p>Não existem chamados</p>
+        @endforelse
     </div>
 </div>
 @endsection
